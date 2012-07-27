a373f26ab2ae9bc04abdbe7e69a2cda1bc458c2c
jmztaylor <jmztaylor@gmail.com>
2012-07-27 13:31:50 -0300

    Update primoc-vendor-blobs.mk

--- primoc-vendor-blobs.mk
+++ primoc-vendor-blobs.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/libaudioalsa.so:/system/lib/libaudioalsa.so \
     vendor/htc/primoc/proprietary/lib/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
-	vendor/htc/primoc/proprietary/etc/soundimage/Sound_Bass_Booster.txt:system/etc/soundimage/Sound_Bass_Booster.txt \
+    vendor/htc/primoc/proprietary/etc/soundimage/Sound_Bass_Booster.txt:system/etc/soundimage/Sound_Bass_Booster.txt \
     vendor/htc/primoc/proprietary/etc/soundimage/Sound_Blues.txt:system/etc/soundimage/Sound_Blues.txt \
     vendor/htc/primoc/proprietary/etc/soundimage/Sound_Classical.txt:system/etc/soundimage/Sound_Classical.txt \
     vendor/htc/primoc/proprietary/etc/soundimage/Sound_Country.txt:system/etc/soundimage/Sound_Country.txt \
@@ -45,76 +45,76 @@ PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/etc/soundimage/Sound_SRS_V_SPK.txt:system/etc/soundimage/Sound_SRS_V_SPK.txt \
     vendor/htc/primoc/proprietary/etc/soundimage/Sound_Treble_Booster.txt:system/etc/soundimage/Sound_Treble_Booster.txt \
     vendor/htc/primoc/proprietary/etc/soundimage/Sound_Vocal_Booster.txt:system/etc/soundimage/Sound_Vocal_Booster.txt \
-	vendor/htc/primoc/proprietary/etc/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    vendor/htc/primoc/proprietary/etc/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
     vendor/htc/primoc/proprietary/etc/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
     vendor/htc/primoc/proprietary/etc/AudioBTID.csv:system/etc/AudioBTID.csv \
     vendor/htc/primoc/proprietary/etc/audio_effects.conf.csv:system/etc/audio_effects.conf \
     vendor/htc/primoc/proprietary/etc/CodecDSPID.txt:system/etc/CodecDSPID.txt \
     vendor/htc/primoc/proprietary/etc/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
     vendor/htc/primoc/proprietary/etc/firmware/default.acdb:system/etc/firmware/default.acdb \
-	vendor/htc/primoc/proprietary/etc/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
-	vendor/htc/primoc/proprietary/etc/firmware/default_org_wb.acdb:system/etc/firmware/default_org_wb.acdb \
-	vendor/htc/primoc/proprietary/lib/libaudioflinger.so:/system/lib/libaudioflinger.so
+    vendor/htc/primoc/proprietary/etc/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
+    vendor/htc/primoc/proprietary/etc/firmware/default_org_wb.acdb:system/etc/firmware/default_org_wb.acdb \
+    vendor/htc/primoc/proprietary/lib/libaudioflinger.so:/system/lib/libaudioflinger.so
 	
 # Wifi/BT firmware
 PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/etc/firmware/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
-	vendor/htc/primoc/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b1.bin:system/etc/firmware/fw_bcm4330_apsta_b1.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b1.bin:system/vendor/firmware/fw_bcm4330_apsta_b1.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/etc/firmware/fw_bcm4330_apsta_b2.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/vendor/firmware/fw_bcm4330_apsta_b2.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b2.bin:system/etc/firmware/fw_bcm4330_b2.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b2.bin:system/vendor/firmware/fw_bcm4330_b2.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b1.bin:system/etc/firmware/fw_bcm4330_b1.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b1.bin:system/vendor/firmware/fw_bcm4330_b1.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/etc/firmware/fw_bcm4330_p2p_b2.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/vendor/firmware/fw_bcm4330_p2p_b2.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b1.bin:system/etc/firmware/fw_bcm4330_p2p_b1.bin \
-	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b1.bin:system/vendor/firmware/fw_bcm4330_p2p_b1.bin
+    vendor/htc/primoc/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b1.bin:system/etc/firmware/fw_bcm4330_apsta_b1.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b1.bin:system/vendor/firmware/fw_bcm4330_apsta_b1.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/etc/firmware/fw_bcm4330_apsta_b2.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/vendor/firmware/fw_bcm4330_apsta_b2.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b2.bin:system/etc/firmware/fw_bcm4330_b2.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b2.bin:system/vendor/firmware/fw_bcm4330_b2.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b1.bin:system/etc/firmware/fw_bcm4330_b1.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b1.bin:system/vendor/firmware/fw_bcm4330_b1.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/etc/firmware/fw_bcm4330_p2p_b2.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/vendor/firmware/fw_bcm4330_p2p_b2.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b1.bin:system/etc/firmware/fw_bcm4330_p2p_b1.bin \
+    vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b1.bin:system/vendor/firmware/fw_bcm4330_p2p_b1.bin
 	
 # HW files
 PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so \
-	vendor/htc/primoc/proprietary/lib/hw/audio.primary.default.so:system/lib/hw/audio.primary.default.so \
-	vendor/htc/primoc/proprietary/lib/hw/audio.primary.msm7x30.so:system/lib/hw/audio.primary.msm7x30.so \
-	vendor/htc/primoc/proprietary/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default.so \
-	vendor/htc/primoc/proprietary/lib/hw/audio_policy.msm7x30.so:system/lib/hw/audio_policy.msm7x30.so \
-	vendor/htc/primoc/proprietary/lib/hw/camera.default.so:system/lib/hw/camera.default.so
+    vendor/htc/primoc/proprietary/lib/hw/audio.primary.default.so:system/lib/hw/audio.primary.default.so \
+    vendor/htc/primoc/proprietary/lib/hw/audio.primary.msm7x30.so:system/lib/hw/audio.primary.msm7x30.so \
+    vendor/htc/primoc/proprietary/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default.so \
+    vendor/htc/primoc/proprietary/lib/hw/audio_policy.msm7x30.so:system/lib/hw/audio_policy.msm7x30.so \
+    vendor/htc/primoc/proprietary/lib/hw/camera.default.so:system/lib/hw/camera.default.so
 	
 # EGL
 PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
-	vendor/htc/primoc/proprietary/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
-	vendor/htc/primoc/proprietary/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
-	vendor/htc/primoc/proprietary/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
-	vendor/htc/primoc/proprietary/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so
+    vendor/htc/primoc/proprietary/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
+    vendor/htc/primoc/proprietary/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/htc/primoc/proprietary/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
+    vendor/htc/primoc/proprietary/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so
 
 # Camera
 PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/bin/awb_camera:/system/bin/awb_camera \
-	vendor/htc/primoc/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
-	vendor/htc/primoc/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
-	vendor/htc/primoc/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
-	vendor/htc/primoc/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
-	vendor/htc/primoc/proprietary/lib/libcentralZoomTransition.so:system/lib/libcentralZoomTransition.so \
-	vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_default_video.so:system/lib/libchromatix_s5k4e5yx_default_video.so \
-	vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_hdr.so:system/lib/libchromatix_s5k4e5yx_hdr.so \
-	vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
-	vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_hfr_4x.so:system/lib/libchromatix_s5k4e5yx_hfr_4x.so \
-	vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
-	vendor/htc/primoc/proprietary/lib/libcubicRotateTransition.so:system/lib/libcubicRotateTransition.so \
-	vendor/htc/primoc/proprietary/lib/libzoomTransition.so:system/lib/libzoomTransition.so \
-	vendor/htc/primoc/proprietary/lib/libmmipl.so:/system/lib/libmmipl.so \
-	vendor/htc/primoc/proprietary/lib/libmmjpeg.so:/system/lib/libmmjpeg.so \
-	vendor/htc/primoc/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
-	vendor/htc/primoc/proprietary/lib/libmmcamera_rawchipproc.so:system/lib/libmmcamera_rawchipproc.so \
-	vendor/htc/primoc/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
-	vendor/htc/primoc/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
-	vendor/htc/primoc/proprietary/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
-	vendor/htc/primoc/proprietary/bin/lsc_camera:/system/bin/lsc_camera \
-	vendor/htc/primoc/proprietary/lib/liboemcamera.so:/system/lib/liboemcamera.so \
-	vendor/htc/primoc/proprietary/lib/libmmparser_lite.so:system/lib/libmmparser_lite.so
+    vendor/htc/primoc/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
+    vendor/htc/primoc/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
+    vendor/htc/primoc/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
+    vendor/htc/primoc/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
+    vendor/htc/primoc/proprietary/lib/libcentralZoomTransition.so:system/lib/libcentralZoomTransition.so \
+    vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_default_video.so:system/lib/libchromatix_s5k4e5yx_default_video.so \
+    vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_hdr.so:system/lib/libchromatix_s5k4e5yx_hdr.so \
+    vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
+    vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_hfr_4x.so:system/lib/libchromatix_s5k4e5yx_hfr_4x.so \
+    vendor/htc/primoc/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
+    vendor/htc/primoc/proprietary/lib/libcubicRotateTransition.so:system/lib/libcubicRotateTransition.so \
+    vendor/htc/primoc/proprietary/lib/libzoomTransition.so:system/lib/libzoomTransition.so \
+    vendor/htc/primoc/proprietary/lib/libmmipl.so:/system/lib/libmmipl.so \
+    vendor/htc/primoc/proprietary/lib/libmmjpeg.so:/system/lib/libmmjpeg.so \
+    vendor/htc/primoc/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
+    vendor/htc/primoc/proprietary/lib/libmmcamera_rawchipproc.so:system/lib/libmmcamera_rawchipproc.so \
+    vendor/htc/primoc/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+    vendor/htc/primoc/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    vendor/htc/primoc/proprietary/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
+    vendor/htc/primoc/proprietary/bin/lsc_camera:/system/bin/lsc_camera \
+    vendor/htc/primoc/proprietary/lib/liboemcamera.so:/system/lib/liboemcamera.so \
+    vendor/htc/primoc/proprietary/lib/libmmparser_lite.so:system/lib/libmmparser_lite.so
 
 # Ril
 PRODUCT_COPY_FILES += \
@@ -126,21 +126,21 @@ PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/libnetmgr.so:/system/lib/libnetmgr.so \
     vendor/htc/primoc/proprietary/lib/libdsutils.so:/system/lib/libdsutils.so \
     vendor/htc/primoc/proprietary/lib/libqdp.so:/system/lib/libqdp.so \
-	vendor/htc/primoc/proprietary/lib/libhtc_ril.so:/system/lib/libhtc_ril.so
+    vendor/htc/primoc/proprietary/lib/libhtc_ril.so:/system/lib/libhtc_ril.so
 	
 # Random Crap..Verify necessity 
 PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/bin/bma150_usr:/system/bin/bma150_usr \
-	vendor/htc/primoc/proprietary/bin/logcat2:/system/bin/logcat2 \
-	vendor/htc/primoc/proprietary/bin/rmt_storage:/system/bin/rmt_storage \
-	vendor/htc/primoc/proprietary/bin/zcb:/system/bin/zcb \
-	vendor/htc/primoc/proprietary/lib/libC2D2.so:/system/lib/libC2D2.so \
-	vendor/htc/primoc/proprietary/lib/libgemini.so:/system/lib/libgemini.so \
-	vendor/htc/primoc/proprietary/lib/libgsl.so:/system/lib/libgsl.so \
-	vendor/htc/primoc/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
-	vendor/htc/primoc/proprietary/lib/libOpenVG.so:/system/lib/libOpenVG.so \
-	vendor/htc/primoc/proprietary/lib/libposteffect.so:/system/lib/libposteffect.so \
-	vendor/htc/primoc/proprietary/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
+    vendor/htc/primoc/proprietary/bin/logcat2:/system/bin/logcat2 \
+    vendor/htc/primoc/proprietary/bin/rmt_storage:/system/bin/rmt_storage \
+    vendor/htc/primoc/proprietary/bin/zcb:/system/bin/zcb \
+    vendor/htc/primoc/proprietary/lib/libC2D2.so:/system/lib/libC2D2.so \
+    vendor/htc/primoc/proprietary/lib/libgemini.so:/system/lib/libgemini.so \
+    vendor/htc/primoc/proprietary/lib/libgsl.so:/system/lib/libgsl.so \
+    vendor/htc/primoc/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
+    vendor/htc/primoc/proprietary/lib/libOpenVG.so:/system/lib/libOpenVG.so \
+    vendor/htc/primoc/proprietary/lib/libposteffect.so:/system/lib/libposteffect.so \
+    vendor/htc/primoc/proprietary/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
     vendor/htc/primoc/proprietary/lib/libv8.so:system/lib/libv8.so
 	
 	
