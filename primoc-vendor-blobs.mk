db1b913c172b795dcae2ed7f57281648d8284ddc
Jmz <jmztaylor@gmail.com>
2012-07-27 08:07:46 -0400

    fixing wifi

--- primoc-vendor-blobs.mk
+++ primoc-vendor-blobs.mk
@@ -28,6 +28,10 @@ PRODUCT_COPY_FILES += \
 	vendor/htc/primoc/proprietary/bin/rmt_storage:/system/bin/rmt_storage \
 	vendor/htc/primoc/proprietary/bin/zcb:/system/bin/zcb \
 	vendor/htc/primoc/proprietary/etc/firmware/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
+	vendor/htc/primoc/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
+	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/etc/firmware/fw_bcm4330_apsta_b2.bin \
+	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_b2.bin:system/etc/firmware/fw_bcm4330_b2.bin \
+	vendor/htc/primoc/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/etc/firmware/fw_bcm4330_p2p_b2.bin \
     vendor/htc/primoc/proprietary/etc/firmware/default.acdb:system/etc/firmware/default.acdb \
 	vendor/htc/primoc/proprietary/etc/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
 	vendor/htc/primoc/proprietary/etc/firmware/default_org_wb.acdb:system/etc/firmware/default_org_wb.acdb \
