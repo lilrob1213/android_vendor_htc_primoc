a11722945fd15ffe4bdeeada25a9d9efd33b5e06
Jmz <jmztaylor@gmail.com>
2012-08-08 19:17:09 -0400

    because they want to break shit in source

--- primoc-vendor-blobs.mk
+++ primoc-vendor-blobs.mk
@@ -59,6 +59,7 @@ PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/hw/audio.primary.default.so:system/lib/hw/audio.primary.default.so \
     vendor/htc/primoc/proprietary/lib/hw/audio.primary.msm7x30.so:system/lib/hw/audio.primary.msm7x30.so \
     vendor/htc/primoc/proprietary/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default.so \
+    vendor/htc/primoc/proprietary/lib/hw/hwcomposer.msm7x30.so:system/lib/hw/hwcomposer.msm7x30.so \
     vendor/htc/primoc/proprietary/lib/hw/camera.default.so:system/lib/hw/camera.default.so
 	
 # EGL
