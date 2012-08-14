f374b7ec8c105f5f91cb8edcd801575b8fda6ff1
Jmz <jmztaylor@gmail.com>
2012-08-14 16:54:11 -0400

    removed and build from source

--- primoc-vendor-blobs.mk
+++ primoc-vendor-blobs.mk
@@ -93,7 +93,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/libhtc_ril.so:/system/lib/libhtc_ril.so \
     vendor/htc/primoc/proprietary/lib/libidl.so:/system/lib/libidl.so \
     vendor/htc/primoc/proprietary/lib/libnetmgr.so:/system/lib/libnetmgr.so \
-    vendor/htc/primoc/proprietary/lib/libril.so:/system/lib/libril.so \
     vendor/htc/primoc/proprietary/lib/libqdp.so:/system/lib/libqdp.so \
     vendor/htc/primoc/proprietary/lib/libqmi.so:/system/lib/libqmi.so
 
